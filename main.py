f4cf7369fb942d8b476ebf285e36ddaddc9a4d6d
maxsnax <max.ed.schumacher@gmail.com>
2025-04-27 06:12:29 -0700

    Changed output messages for chest collection logs. Optimized click collection to wait 30 mins instead of check every 60s. Changed default chest check to 15s

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
 import time
-import pygame
 import pyautogui
-import argparse
+from PIL import Image
 import json
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"  # remove python console import message
+import pygame
 
 # ==================================================================
 # Configuration & Globals
 # ==================================================================
-play_sound = False
+beep_count = 0
 click_chest = False
-check_interval = 60  # seconds
+check_interval = 15  # seconds
+last_collection = 0
 
-count = 0
-collected = 0
+check_count = 0
+collected_count = 0
 
 BASE_DIR = os.path.dirname(__file__)
 CHEST_IMAGE = os.path.join(BASE_DIR, 'images', 'bongo_chest.png')
@@ -27,7 +29,7 @@ _last_settings_mtime = None
 # Helper Functions
 # ==================================================================
 def load_settings():
-    global play_sound, click_chest, _last_settings_mtime
+    global beep_count, click_chest, _last_settings_mtime
 
     try:
         mtime = os.path.getmtime(SETTINGS_FILE)
@@ -35,70 +37,115 @@ def load_settings():
             _last_settings_mtime = mtime
             with open(SETTINGS_FILE, 'r') as f:
                 data = json.load(f)
-                play_sound = data.get('play_sound', play_sound)
+                beep_count = data.get('beep_count', beep_count)
                 click_chest = data.get('click_chest', click_chest)
-            print(f"[Settings Reloaded] play_sound={play_sound}, click_chest={click_chest}")
+            settings_text = f"[ Settings: beep_count={beep_count} click_chest={str(bool(click_chest)):<5} ]"
+            print('-' * len(settings_text))
+            print(settings_text)
+            print('-' * len(settings_text))
+
     except Exception as e:
         print(f"[Settings Error] {e}")
 
 
-def take_screenshot(path: str):
-    screenshot = pyautogui.screenshot()
-    screenshot.save(path)
-    return screenshot
+def take_screenshot() -> Image.Image:
+    return pyautogui.screenshot()
 
 
-def find_chest(screenshot_path: str):
-    return pyautogui.locate(CHEST_IMAGE, screenshot_path, grayscale=True, confidence=0.85)
+def find_chest(screenshot):
+    global check_count
+    global beep_count
+
+    check_count += 1
+    found_box = pyautogui.locate(CHEST_IMAGE, screenshot, grayscale=True, confidence=0.85)
+
+    log_status(bool(found_box))
+
+    if found_box:  # return the coordinates of the center of the image
+        play_beep(beep_count)
+        center_x = found_box.left + found_box.width // 2
+        center_y = found_box.top + found_box.height // 2
+        return center_x, center_y
+
+    return None
 
 
-def play_beep():
-    pygame.mixer.init()  # Initialize the mixer
-    for x in range(3):
+def play_beep(n):
+    for x in range(n):  # play beep sound n times
         pygame.mixer.Sound(SOUND_PATH).play()
-        time.sleep(0.5)  # Wait for the sound to finish playing
+        time.sleep(0.5)  # wait for sound to finish playing
 
 
 def log_status(found: bool):
     timestamp = time.strftime("%H:%M:%S")
-    print(f"#{collected}/{count} {timestamp}:", end=" ")
-    print("Found chest" if found else "No chest")
+    status = "Found" if found else "No chest"
+
+    # format the print statement with specific width for each part
+    print(f"[ Check #{check_count:<7} {timestamp:<14} {status:<10} ]")
 
 
 def process_chest():
-    global collected
-    take_screenshot(SCREENSHOT_PATH)
-    match = find_chest(SCREENSHOT_PATH)
+    global collected_count
+    global beep_count
+    global check_interval
+    global click_chest
+    global last_collection
 
-    log_status(bool(match))
+    screenshot = take_screenshot()
+    match = find_chest(screenshot)
 
     if match:
-        collected += 1
-        center_x = match.left + 10
-        center_y = match.top + 10
-
-        if play_sound:
-            play_beep()
 
         if click_chest:
-            pyautogui.moveTo(center_x, center_y)
+            play_beep(beep_count)
+            pyautogui.moveTo(match, duration=0.2)  # duration helps with click detection on chest
             pyautogui.click()
+
+            # check to make sure the chest is collected
+            time.sleep(1)
+            screenshot = take_screenshot()
+            match = find_chest(screenshot)
+
+            # match indicates unsuccessful chest collection
+            if match:
+                check_interval = 5  # give 5s between next click attempt
+            # no match means chest collected
+            else:
+                last_collection = time.time()
+                collected_count += 1
+                check_interval = 1800  # check again for next chest in 30 minutes
+                print(f"[ Chest Collected: #{collected_count:<22} ]")
+
+        return True
+
+    # if chest hasn't collected >30mins then reset check interval
+    elif time.time() - last_collection > 1800:
+        check_interval = 15
+
+    return False
 
 
 # ==================================================================
 # Main Loop
 # ==================================================================
 def main():
-    global count
+    global check_count
+    global last_collection
 
-    play_beep()
+    # set the last collection to now for 30min interval checks later
+    last_collection = time.time()
+
+    pygame.mixer.init()
+    play_beep(3)
 
     while True:
         load_settings()
-        count += 1
         process_chest()
         time.sleep(check_interval)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n[Exit]")
